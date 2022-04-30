ddd0b9147ce13eed379c2e1b7dc79de853b95dc8
victor19pa <vpinedaa@unah.hn>
2022-04-30 17:32:15 -0600

    :sparkles: feat: modal @materia-ui

--- src/components/Receta.jsx
+++ src/components/Receta.jsx
@@ -1,7 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ModalContext } from "../context/ModalContext";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const Receta = ({ receta }) => {
+  //config modal
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+    setIdReceta(null);
+  };
+
   const { setIdReceta } = useContext(ModalContext);
 
   return (
@@ -21,10 +44,27 @@ const Receta = ({ receta }) => {
             className="btn btn-block btn-primary"
             onClick={() => {
               setIdReceta(receta.idDrink);
+              handleOpen();
             }}
           >
             Ver Receta
           </button>
+
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Text in a modal
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              </Typography>
+            </Box>
+          </Modal>
         </div>
       </div>
     </div>
