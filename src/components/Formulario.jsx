ba684576344e5a19e6382467f01bf55825dadbd3
victor19pa <vpinedaa@unah.hn>
2022-04-30 14:50:40 -0600

    :sparkles: feat: formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
 const Formulario = () => {
-  return <h1>Formulario Aqui</h1>;
+  return (
+    <form action="" className="col-12">
+      <fieldset className="text-center">
+        <legend>Busca bebidas por categoria o ingrediente</legend>
+      </fieldset>
+
+      <div className="row mt-4">
+        <div className="col-md-4">
+          <input
+            type="text"
+            className="form-control"
+            name="nombre"
+            placeholder="Buscar por Ingrediente"
+          />
+        </div>
+
+        <div className="col-md-4">
+          <select name="categoria" id="categoria" className="form-control">
+            <option value="">--Selecciona una Categoria </option>
+          </select>
+        </div>
+
+        <div className="col-md-4">
+          <input
+            type="submit"
+            className="btn btn-block btn-primary"
+            value="Buscar Bebidas"
+          />
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default Formulario;
