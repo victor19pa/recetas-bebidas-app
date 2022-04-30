3994917a6626fc35ac0aa4ee293723dd7c735720
victor19pa <vpinedaa@unah.hn>
2022-04-30 16:35:55 -0600

    :sparkles: feat: get recipe by id api

--- src/context/ModalContext.jsx
+++ src/context/ModalContext.jsx
@@ -9,14 +9,27 @@ export const ModalContext = createContext();
 const ModalProvider = (props) => {
   //crear state del context
   const [idReceta, setIdReceta] = useState(null);
+  const [receta, setReceta] = useState({});
 
-  //ejecutar llamado a api
-  // useEffect(() => {}, []);
+  //ejecutar llamado a api teniendo id de receta
+  useEffect(() => {
+    const obtenerReceta = async () => {
+      if (!idReceta) return;
+
+      const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idReceta}`;
+
+      const resultado = await axios.get(url);
+
+      setReceta(resultado.data.drinks[0]);
+    };
+    obtenerReceta();
+  }, [idReceta]);
 
   return (
     <ModalContext.Provider
       value={{
         setIdReceta,
+        receta,
       }}
     >
       {props.children}
